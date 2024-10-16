3a82524ca96f890f6938c8498fb0d95a29c5c64b
madelinefruin <132620519+madelinefruin@users.noreply.github.com>
2024-10-16 11:10:46 -0700

    Update main.py
    Has working code. Has EC code.

--- main.py
+++ main.py
@@ -13,6 +13,14 @@ led_pins = [
     board.IO21,
     board.IO26, # type: ignore
     board.IO47,
+    board.IO33, # type: ignore
+    board.IO34, # type: ignore
+    board.IO48,
+    board.IO35,
+    board.IO36,
+    board.IO37,
+    board.IO38,
+    board.IO39,
     # do the rest...
 ]
 
@@ -25,12 +33,26 @@ for led in leds:
 while True:
     volume = microphone.value
 
-    print(volume)
+    #print(volume)
 
-    leds[0].value = not leds[0].value
-    leds[1].value = not leds[0].value
+    for x in range (0,11):
+        if volume > 25000 + (x*2000):
+           leds[x].value = 1
+        else:
+            break
+    
+    for x in reversed(range(0,11)):
+        pvol = volume
+        volume = microphone.value
+        if pvol<volume:
+            break
+        if leds[x].value == 1: 
+            if volume < (25000 + (x*2000)):
+                print(volume)
+                sleep(.05)
+                leds[x].value = 0
 
-    sleep(1)
+
 
     # instead of blinking,
     # how can you make the LEDs
